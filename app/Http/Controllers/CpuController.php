bf3bc5c89412a373a94ca6f00ec4748683fc3b9e
spghljh <spghljh@gmail.com>
2025-08-11 18:08:32 +0900

    데이터베이스 인터페이스 및 CSS 수정 중

--- app/Http/Controllers/CpuController.php
+++ app/Http/Controllers/CpuController.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
+use App\Models\Mcl;
 use Illuminate\Http\Request;
 
 class CpuController extends Controller
 {
     public function index()
     {
-        // MySQL의 mcl 테이블에서 CPU 리스트 가져오기
-        $cpus = DB::table('mcl')->orderBy('id_cpu', 'asc')->get();
+        // Eloquent로 CPU 리스트 가져오기
+        $cpus = Mcl::orderBy('id_cpu', 'asc')->get();
 
         // Blade 뷰에 데이터 전달
         return view('modules.box.cpu.module_box-cpu_list', compact('cpus'));
